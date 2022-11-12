1aa8c6fa006dc41145585dd89854d1d92ec995cc
pihujindal <110271241+pihujindal@users.noreply.github.com>
2022-11-12 19:29:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var weight=[30,34,40,42];
+
+function setup() 
+{
+  createCanvas(400,400);
+  var sum=weight[0]+weight[1]+weight[2]+weight[3];
+var avg=sum/weight.length;
+console.log(avg);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
